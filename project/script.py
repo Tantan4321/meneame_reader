b0eb84bec8203f50548bfef48d37e31f48d5e45b
Eastan Giebler <kupa528@yahoo.com>
2019-12-11 14:42:08 -0600

    Fix: print out newly arrived duplicate items
    - new issue: first run of script duplicates latest item

--- project/script.py
+++ project/script.py
@@ -5,18 +5,26 @@ from project import firehose_url
 
 def run_reader():
     last_timestamp = 0
+    same_ts_count = 0  # keep track of number of events with last_timestamp
     while True:
         r = requests.get(firehose_url)
         data = r.json()  # parse response into json
 
         events_list = data["events"]  # parse events item into list
         reversed_events = events_list[::-1]  # reverse the list to be chronologically correct
+        ts_duplicate_counter = 0  # unique events with same ts can show up next query
 
         for event in reversed_events:
-            if int(event["ts"]) <= int(last_timestamp):
+            if int(event["ts"]) < int(last_timestamp):
                 continue
+            if int(event["ts"]) == int(last_timestamp):
+                ts_duplicate_counter += 1
+                if ts_duplicate_counter <= same_ts_count:
+                    continue  # this would be a duplicate event, continue
+
             print(event["ts"])
 
+        same_ts_count = ts_duplicate_counter
         last_timestamp = data["ts"]  # track last timestamp for next run
 
 
