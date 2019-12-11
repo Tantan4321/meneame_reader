a9f175d9cdf5a866b6d978e708432ca2be8713d5
Eastan Giebler <kupa528@yahoo.com>
2019-12-11 14:11:25 -0600

    Live feed in chronological order

--- project/script.py
+++ project/script.py
@@ -7,9 +7,17 @@ def run_reader():
     last_timestamp = 0
     while True:
         r = requests.get(firehose_url)
-        data = r.json()  # TODO: filter json input for only new timestamps (reduce loop time)
-        print(data)
+        data = r.json()  # parse response into json
 
+        events_list = data["events"]  # parse events item into list
+        reversed_events = events_list[::-1]  # reverse the list to be chronologically correct
+
+        for event in reversed_events:
+            if int(event["ts"]) <= int(last_timestamp):
+                continue
+            print(event["ts"])
+
+        last_timestamp = data["ts"]  # track last timestamp for next run
 
 
 def print_entry(timestamp, sub, action):
