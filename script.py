24d044ef60d5fdeaec822169a2375eb9e4533d3d
Eastan Giebler <kupa528@yahoo.com>
2020-01-11 14:26:40 -0600

    Rework database reader logic

--- script.py
+++ script.py
@@ -14,8 +14,9 @@ def exit_handler(connection):
 
 def run_reader():
     last_timestamp = 0
-    same_ts_count = 0  # keep track of number of events with last_timestamp
-    ts_duplicate_counter = 1  # unique events with same ts can show up next query, init with 1
+    is_first_run = True
+    last_events = []
+
     try:
         requests.get("https://www.google.com/")
     except Exception:
@@ -24,6 +25,8 @@ def run_reader():
 
     con = db_utils.db_connect()  # connect to sqlite database
     atexit.register(exit_handler, con)
+
+    latest_events = []
     try:
         while True:
             r = requests.get("https://www.meneame.net/backend/sneaker2")
@@ -32,24 +35,37 @@ def run_reader():
             events_list = data["events"]  # parse events item into list
             reversed_events = events_list[::-1]  # reverse the list to be chronologically correct
 
-            for event in reversed_events:
-                if int(event["ts"]) < int(last_timestamp):  # entry is before latest timestamp, already logged
-                    continue
-                if int(event["ts"]) == int(last_timestamp):  # new entries with latest timestamp could have arrived
-                    ts_duplicate_counter += 1
-                    if ts_duplicate_counter <= same_ts_count:
-                        continue  # this would be a duplicate event, continue
+            latest_events.clear()  # clear the local latest events tracker
 
-                log_entry(con, event)
-                last_timestamp = event["ts"]  # track last timestamp for next run
+            if is_first_run:
+                last_timestamp = int(events_list[0]["ts"])  # first element of reversed list will be latest event
+                for event in reversed_events:
+                    if int(event["ts"]) == int(last_timestamp):
+                        latest_events.append(event)
+                    log_entry(con, event)
 
-            same_ts_count = ts_duplicate_counter  # track number of entries with latest timestamp for next run
-            ts_duplicate_counter = 0  # reset ts counter for next loop
+                last_events = latest_events.copy()  # copy local latest events into last events
+                is_first_run = False
+            else:
+                new_timestamp = int(events_list[0]["ts"])  # first element of reversed list will be the most recent
+                for event in reversed_events:
+                    if int(event["ts"]) == int(new_timestamp):  # keep track of latest events for next run
+                        latest_events.append(event)
+                    if int(event["ts"]) < int(last_timestamp):  # entry is before latest timestamp, already logged
+                        continue
+                    if int(event["ts"]) == int(last_timestamp):  # new entries with latest timestamp could have arrived
+                        if event in last_events:
+                            continue  # this event was reported last run, continue
+                    log_entry(con, event)
+
+                last_events = latest_events.copy()  # copy local latest events into last events
+                last_timestamp = new_timestamp  # track last timestamp for next run
+
     except KeyboardInterrupt:
-        db_utils.print_table(con)
+        print('Keyboard interrupt...')
 
 
-def print_entry(time, sub_name, action, votes, comments, title, user, status):
+def print_entry(time, sub_name, action, votes: str, comments: str, title, user, status):
     vote_comments = str(votes) + "/" + str(comments)
 
     print(str(BeautifulSoup("{} | {:<12} | {:<10} | {:<8} | {} | {} | {}"
@@ -61,18 +77,21 @@ def log_entry(connection, event):
     sub_name = event["sub_name"]
     action = str(event["type"]).strip()
     votes = event["votes"]
-    comments = event["com"]
+    comments = str(event["com"]).strip()
     title = event["title"]
     user = event["who"]
     status = event["status"]
 
-    if action == 'new':
-        db_utils.create_article(connection, title, user, sub_name, time)
+    # if action == 'new':
+    #     db_utils.create_article(connection, title, user, sub_name, time)
+    #
+    # if action == 'vote':
+    #     db_utils.update_votes(connection, title, int(votes))
+    #
+    # if action == 'comment':
+    #     db_utils.update_comments(connection, title, int(comments))
 
-    if action == 'vote':
-        db_utils.update_votes(connection, title, votes)
-
-    print_entry(time, sub_name, action, votes, comments, title, user, status)
+    print_entry(time, sub_name, action, votes, comments, title, user, status)  # print the entry in CL
 
 
 if __name__ == "__main__":
