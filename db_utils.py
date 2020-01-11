d7c491316093bd8a5dc8ec461a7b918e5a376575
Eastan Giebler <kupa528@yahoo.com>
2020-01-11 01:49:33 -0600

    database manipulation methods

--- db_utils.py
+++ db_utils.py
@@ -8,3 +8,22 @@ DEFAULT_PATH = os.path.join(os.path.dirname(__file__), 'database.sqlite3')
 def db_connect(db_path: str = DEFAULT_PATH):
     con = sqlite3.connect(db_path)
     return con
+
+
+def create_article(connection, title, username, sub, timestamp):
+    sql = """
+        INSERT INTO articles (title, votes, user, sub, timestamp) 
+        VALUES (?,?,?,?,?)"""
+
+    cur = connection.cursor()
+    cur.execute(sql, (title, 0, username, sub, timestamp))
+    return cur.lastrowid
+
+
+def update_votes(connection, title, votes):
+    sql = """
+            UPDATE articles SET votes = ? WHERE title = ?"""
+
+    cur = connection.cursor()
+    cur.execute(sql, (votes, title))
+    return cur.lastrowid
